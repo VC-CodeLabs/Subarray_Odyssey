6e3eddd3a2570b01b53abbbcf5832302f4979e7d
VividCloud <154607214+VC-CodeLabs@users.noreply.github.com>
2024-01-20 17:55:19 -0500

    JavaScript template

---
+++ [name]_solution.js
@@ -0,0 +1,7 @@
+function maxSubArray(nums) {
+    // Your implementation goes here
+    return 0; // placeholder
+}
+
+const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+console.log(`Maximum subarray sum is: ${maxSubArray(nums)}`);
