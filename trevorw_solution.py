3fc19f3b35f4ca10f4ef0af57c14971c459169d6
merestar <105946788+merestar@users.noreply.github.com>
2024-01-28 22:20:20 -0500

    Update and rename [name]_solution.py to trevorw_solution.py

--- trevorw_solution.py
+++ trevorw_solution.py
@@ -1,6 +1,6 @@
 def max_sub_array(nums):
     # Your implementation goes here
-    return 0 # placeholder
+    return max([{"Sum": sum(nums[a: b + 1]), "Subarray Indices": [a, b]} for a in range(len(nums)) for b in range(len(nums)) if a <= b], key=lambda item: item["Sum"])
 
 if __name__ == "__main__":
     nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
