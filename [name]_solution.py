11ea548793530ed12ed17322cd495c2009721fc6
merestar <105946788+merestar@users.noreply.github.com>
2024-01-28 22:23:42 -0500

    Create [name]_solution.py

---
+++ [name]_solution.py
@@ -0,0 +1,7 @@
+def max_sub_array(nums):
+    # Your implementation goes here
+    return 0 # placeholder
+
+if __name__ == "__main__":
+    nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    print(f"Maximum subarray sum is: {max_sub_array(nums)}")
