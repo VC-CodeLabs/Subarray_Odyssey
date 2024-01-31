328298eba9326449465f7d11eb5bb670185e6753
VividCloud <154607214+VC-CodeLabs@users.noreply.github.com>
2024-01-31 11:58:43 -0500

    Delete [name]_solution.rs

--- [name]_solution.rs
+++
@@ -1,9 +0,0 @@
-fn max_sub_array(nums: Vec<i32>) -> i32 {
-    // Your implementation goes here
-    0 // placeholder
-}
-
-fn main() {
-    let nums = vec![-2, 1, -3, 4, -1, 2, 1, -5, 4];
-    println!("Maximum subarray sum is: {}", max_sub_array(nums));
-}
